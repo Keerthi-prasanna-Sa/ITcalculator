19b72954d5daf9ac26b985e3dd4a4646e710499c
Keerthi prasanna Sa <keerthiprasanna01@gmail.com>
2021-11-24 09:22:50 +0530

    Edited tax_calculation

--- src/calculate_tax.py
+++ src/calculate_tax.py
@@ -2,7 +2,7 @@ def new_tax_calculation(amount):
     if amount <= 250000:  # 1 rupee to 2 Lakh 50 thousand rupees
         tax = 0
     elif amount <= 500000:  # 2 Lakh 50 thousand rupees to 5 Lakh rupees
-        tax = amount * 0.05
+        tax = (amount - 250000) * 0.05
     elif amount <= 750000:  # 5 Lakh rupees to 7 lakh 50 thousand rupees
         tax = (amount - 500000) * 0.10 + 12500
     elif amount <= 1000000:  # 7 lakh 50 thousand rupees to 10 Lakh rupees
@@ -34,7 +34,7 @@ def old_tax_calculation(amount, age):
         if amount <= 300000:  # 1 rupee to 3 Lakh rupees
             tax = 0
         elif amount <= 500000:  # 3 Lakh rupees to 5 Lakh rupees
-            tax = amount * 0.05
+            tax = (amount - 300000) * 0.05
         elif amount <= 750000:  # 5 Lakh rupees to 7 lakh 50 thousand rupees
             tax = (amount - 500000) * 0.20 + 12500
         elif amount <= 1000000:  # 7 lakh 50 thousand rupees to 10 Lakh rupees
@@ -49,7 +49,7 @@ def old_tax_calculation(amount, age):
         if amount <= 250000:  # 1 rupee to 2 Lakh 50 thousand rupees
             tax = 0
         elif amount <= 500000:  # 2 Lakh 50 thousand rupees to 5 Lakh rupees
-            tax = amount * 0.05
+            tax = (amount - 250000) * 0.05
         elif amount <= 750000:  # 5 Lakh rupees to 7 lakh 50 thousand rupees
             tax = (amount - 500000) * 0.20 + 12500
         elif amount <= 1000000:  # 7 lakh 50 thousand rupees to 10 Lakh rupees
